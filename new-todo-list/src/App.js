0c9160d25dd0ee436d22305c94fc8dd7b9e0e7a4
freddyloboq <freddyloboq@gmail.com>
2022-01-28 01:20:32 +0000

    aqui creare mi nuevo todo list

---
+++ new-todo-list/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        aqui hare mi nuevo todo list
+      </header>
+    </div>
+  );
+}
+
+export default App;
